09b0a9eb6fb7efd7ea4d57c9590987a2b21e3964
+ <+>
2021-09-29 14:29:21 -0400

    added tons of content. Should have done more commits earlier

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "These are a few of my favorite things";
 document.querySelector("body").appendChild(h2);
+
+// const favMovies = document.createElement("div");
+// favMovies.setAttribute('id', 'movies');
+// document.querySelector("body").appendChild(favMovies)
+
+// const favMusic = document.createElement("div");
+// favMusic.setAttribute('id', 'music');
+// document.querySelector("body").appendChild(favMusic);
+
+// const favBooks = document.createElement("div");
+// favBooks.setAttribute('id', 'books');
+// document.querySelector("body").appendChild(favBooks);
+
+// const favMoviesTitle = document.createElement("h3");
+// favMoviesTitle.innerText = "Anna's Favorite Movies";
+// favMovies.appendChild(favMoviesTitle);
+
+
+
+
